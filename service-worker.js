4cb375812d5859fe9e0f59178b835c937de0889e
alstn06052 <alstninvent81@gmail.com>
2026-08-14 16:50:28 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,16 @@
-const CACHE='work-calendar-v8-2-direct';
-const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
-  self.skipWaiting();
-});
-
+const CACHE_RESET='work-calendar-v83-reset';
+self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  );
-  self.clients.claim();
+  event.waitUntil((async()=>{
+    try{
+      const keys=await caches.keys();
+      await Promise.all(keys.map(k=>caches.delete(k)));
+    }catch(e){}
+    try{await self.registration.unregister()}catch(e){}
+    const clientsList=await self.clients.matchAll({type:'window'});
+    for(const client of clientsList){ try{client.navigate(client.url)}catch(e){} }
+  })());
 });
-
 self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET') return;
-
-  if(req.mode==='navigate'){
-    event.respondWith(
-      fetch(req).then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
-        return res;
-      }).catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-
-  if(new URL(req.url).origin===location.origin){
-    event.respondWith(
-      fetch(req).then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE).then(cache=>cache.put(req,copy));
-        return res;
-      }).catch(()=>caches.match(req))
-    );
-  }
+  event.respondWith(fetch(event.request));
 });
