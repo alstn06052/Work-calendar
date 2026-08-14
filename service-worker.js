f01d20b68b436a3a7acac19c8e2c8c9cd45ce502
alstn06052 <alstninvent81@gmail.com>
2026-08-14 16:34:54 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,40 @@
+const CACHE='work-calendar-v8-cloud';
+const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
-const CACHE='work-calendar-v7';
-const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+        return res;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  if(new URL(req.url).origin===location.origin){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put(req,copy));
+        return res;
+      }).catch(()=>caches.match(req))
+    );
+  }
+});
