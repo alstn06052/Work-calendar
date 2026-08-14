ed068b1ae3e240847af068d7e9629e1e3356de90
alstn06052 <alstninvent81@gmail.com>
2026-08-14 16:40:50 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='work-calendar-v8-cloud';
+const CACHE='work-calendar-v8-1-cloud';
 const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',event=>{
